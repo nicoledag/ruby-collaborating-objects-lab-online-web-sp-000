589c76aae0bbc36e95cf3355ff9beed5ff5c161a
Nicole D <nnardella@gmail.com>
2018-11-25 03:18:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,9 @@ attr_accessor :name, :artist
   end
 
   def self.new_by_filename(filename)
-    artist_name, song_name = filename.split(" - ")
-     song = self.new(song_name)
+    split_name = filename.split(" - ")
+    artist_name = split_name[0]
+    song = self.new(split_name[1])
     song.artist_name = artist_name
     song
   end

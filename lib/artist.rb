4d0bdaa673afec66f6c06df8a0ec3334d64db618
Nicole D <nnardella@gmail.com>
2018-11-25 03:24:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,6 @@ attr_accessor :name, :songs
 
   def self.find_or_create_by_name(name)
     self.all.detect {|artist| artist.name == name } || self.new(name).save
-    end
   end
 
   def print_songs

7ceec1c77b66833d8bb47bd6ed5045e8bf07f3e1
Nicole D <nnardella@gmail.com>
2018-11-25 03:14:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,7 @@ attr_accessor :name, :artist
     @name = name
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
 
-  end
 
   def self.new_by_filename(filename)
     split_name = filename.split(" - ")
@@ -20,6 +16,12 @@ attr_accessor :name, :artist
     artist_name = split_name[0]
     song.artist_name = artist_name
     song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+
   end
 
 
